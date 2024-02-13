dde57e00b191f33bf6d625a574156cbf41f58412
Lara Alexander <lara.alexander@hotmail.com>
2024-02-13 08:00:50 -0500

    Build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '978ce4950abf970e307d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '814c94731a5ae7655a84');
