8d2968a25efe0fd224cd908b2df97be1ab7f066d
Lara Alexander <lara.alexander@hotmail.com>
2024-02-17 09:18:01 -0500

    Updated media query

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '814c94731a5ae7655a84');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cc1e92458831a21d0979');
--- src/save.js
+++ src/save.js
@@ -22,47 +22,49 @@ const Save = (props) => {
     } = props;
 
     const blockProps = useBlockProps.save({
-        className: "fa-02-block"
+        className: "fa-02-block-container"
     });
     return (
         <div {...blockProps}>
-            <div class="fa-02-text-content">
-                <RichText.Content
-                    tagName="h2"
-                    className="fa-header-bottom-divider"
-                    value={title}
-                />
-                <RichText.Content
-                    tagName="div"
-                    value={content}
-                />
-                <div class="fa-02-button-container">
-                    <a href={`/${buttonURL}`}><button class="fa-media-emphasis-block-btn wp-block-button__link wp-element-button">
-                        {buttonText ? buttonText + " →" : "Read More →"}
-                    </button></a>
+            <div class="fa-02-block">
+                <div class="fa-02-text-content">
+                    <RichText.Content
+                        tagName="h2"
+                        className="fa-header-bottom-divider"
+                        value={title}
+                    />
+                    <RichText.Content
+                        tagName="div"
+                        value={content}
+                    />
+                    <div class="fa-02-button-container">
+                        <a href={`/${buttonURL}`}><button class="fa-media-emphasis-block-btn wp-block-button__link wp-element-button">
+                            {buttonText ? buttonText + " →" : "Read More →"}
+                        </button></a>
+                    </div>
                 </div>
-            </div>
-            <div class="fa-02-gallery-container">
-                {mediaURL1 && (
-                    <div class="fa-02-image-container" style={{ backgroundImage: `url(${mediaURL1})` }}>
-                        {mediaCaption1 && <span class="inner-caption-span">{mediaCaption1}</span>}
-                    </div>
-                )}
-                {mediaURL2 && (
-                    <div class="fa-02-image-container" style={{ backgroundImage: `url(${mediaURL2})` }}>
-                        {mediaCaption2 && <span class="inner-caption-span">{mediaCaption2}</span>}
-                    </div>
-                )}
-                {mediaURL3 && (
-                    <div class="fa-02-image-container" style={{ backgroundImage: `url(${mediaURL3})` }}>
-                        {mediaCaption3 && <span class="inner-caption-span">{mediaCaption3}</span>}
-                    </div>
-                )}
-                {mediaURL4 && (
-                    <div class="fa-02-image-container" style={{ backgroundImage: `url(${mediaURL4})` }}>
-                        {mediaCaption4 && <span class="inner-caption-span">{mediaCaption4}</span>}
-                    </div>
-                )}
+                <div class="fa-02-gallery-container">
+                    {mediaURL1 && (
+                        <div class="fa-02-image-container" style={{ backgroundImage: `url(${mediaURL1})` }}>
+                            {mediaCaption1 && <span class="inner-caption-span">{mediaCaption1}</span>}
+                        </div>
+                    )}
+                    {mediaURL2 && (
+                        <div class="fa-02-image-container" style={{ backgroundImage: `url(${mediaURL2})` }}>
+                            {mediaCaption2 && <span class="inner-caption-span">{mediaCaption2}</span>}
+                        </div>
+                    )}
+                    {mediaURL3 && (
+                        <div class="fa-02-image-container" style={{ backgroundImage: `url(${mediaURL3})` }}>
+                            {mediaCaption3 && <span class="inner-caption-span">{mediaCaption3}</span>}
+                        </div>
+                    )}
+                    {mediaURL4 && (
+                        <div class="fa-02-image-container" style={{ backgroundImage: `url(${mediaURL4})` }}>
+                            {mediaCaption4 && <span class="inner-caption-span">{mediaCaption4}</span>}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
